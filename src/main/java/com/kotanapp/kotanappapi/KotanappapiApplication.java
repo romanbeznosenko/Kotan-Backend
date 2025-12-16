b904fdb9ecc31d02463443959f8575d6a6ce48df
Roman Beznosenko <r.beznosenko@smartsoftsolutions.pl>
2025-12-16 10:47:34 +0100

    Initial commit

---
+++ src/main/java/com/kotanapp/kotanappapi/KotanappapiApplication.java
@@ -0,0 +1,13 @@
+package com.kotanapp.kotanappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotanappapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KotanappapiApplication.class, args);
+	}
+
+}
