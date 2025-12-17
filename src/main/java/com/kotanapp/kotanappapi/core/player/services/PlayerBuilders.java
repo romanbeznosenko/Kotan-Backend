6808ef943723c931397d49927f47267d6b1c462c
Roman Beznosenko <r.beznosenko@smartsoftsolutions.pl>
2025-12-17 10:50:15 +0100

    Created endpoint for creating players

--- src/main/java/com/kotanapp/kotanappapi/core/player/services/PlayerBuilders.java
+++ src/main/java/com/kotanapp/kotanappapi/core/player/services/PlayerBuilders.java
@@ -3,20 +3,19 @@ package com.kotanapp.kotanappapi.core.player.services;
 import com.kotanapp.kotanappapi.core.player.models.Player;
 import com.kotanapp.kotanappapi.core.player.models.PlayerId;
 import com.kotanapp.kotanappapi.core.player.models.PlayerRequest;
-import com.kotanapp.kotanappapi.core.team.models.Team;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class PlayerBuilders {
-    public static Player buildFromRequest(PlayerRequest playerRequest, Team team) {
+    public static Player buildFromRequest(PlayerRequest playerRequest) {
         return Player.builder()
                 .playerId(PlayerId.of(null))
                 .firstName(playerRequest.firstName())
                 .lastName(playerRequest.lastName())
                 .avatar(null)
                 .playerPosition(playerRequest.playerPosition())
-                .team(team)
+                .team(null)
                 .birthDate(playerRequest.birthDate())
                 .build();
     }
--- src/main/java/com/kotanapp/kotanappapi/core/player/services/PlayerCreateService.java
+++ src/main/java/com/kotanapp/kotanappapi/core/player/services/PlayerCreateService.java
@@ -6,9 +6,7 @@ import com.kotanapp.kotanappapi.core.player.models.Player;
 import com.kotanapp.kotanappapi.core.player.models.PlayerDAO;
 import com.kotanapp.kotanappapi.core.player.models.PlayerRequest;
 import com.kotanapp.kotanappapi.core.team.management.TeamManager;
-import com.kotanapp.kotanappapi.core.team.management.TeamMapper;
 import com.kotanapp.kotanappapi.core.team.management.TeamNotFoundException;
-import com.kotanapp.kotanappapi.core.team.models.Team;
 import com.kotanapp.kotanappapi.core.team.models.TeamDAO;
 import com.kotanapp.kotanappapi.utils.CycleAvoidingMappingContext;
 import lombok.RequiredArgsConstructor;
@@ -24,17 +22,16 @@ public class PlayerCreateService {
     private final PlayerManager playerManager;
     private final PlayerMapper playerMapper;
     private final TeamManager teamManager;
-    private final TeamMapper teamMapper;
 
     public void createPlayer(UUID teamId, PlayerRequest request) {
         log.info("Creating player for team: {}", teamId);
 
         TeamDAO teamDAO = teamManager.findById(teamId)
                 .orElseThrow(TeamNotFoundException::new);
-        Team team = teamMapper.mapToDomain(teamDAO, new CycleAvoidingMappingContext());
 
-        Player player = PlayerBuilders.buildFromRequest(request, team);
+        Player player = PlayerBuilders.buildFromRequest(request);
         PlayerDAO playerDAO = playerMapper.mapToEntity(player, new CycleAvoidingMappingContext());
+        playerDAO.setTeam(teamDAO);
 
         playerManager.saveToDatabase(playerDAO);
     }
