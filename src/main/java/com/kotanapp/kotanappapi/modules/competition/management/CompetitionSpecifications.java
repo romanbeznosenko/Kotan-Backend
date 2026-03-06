80e00d219774856348261c212af89e4e37c85430
Roman Beznosenko <r.beznosenko@smartsoftsolutions.pl>
2026-03-06 12:25:38 +0100

    Created specifications for competitions

---
+++ src/main/java/com/kotanapp/kotanappapi/modules/competition/management/CompetitionSpecifications.java
@@ -0,0 +1,40 @@
+package com.kotanapp.kotanappapi.modules.competition.management;
+
+import com.kotanapp.kotanappapi.modules.competition.models.Competition;
+import com.kotanapp.kotanappapi.modules.competition.models.CompetitionDAO;
+import com.kotanapp.kotanappapi.utils.enums.CompetitionTypeEnum;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.List;
+
+public class CompetitionSpecifications {
+    public static Specification<CompetitionDAO> byName(String name) {
+        return ((root, query, criteriaBuilder) -> {
+            if (name == null) {
+                return criteriaBuilder.conjunction();
+            }
+
+            return criteriaBuilder.like(criteriaBuilder.lower(root.get("name")), "%" + name.toLowerCase() + "%");
+        });
+    }
+
+    public static Specification<CompetitionDAO> bySeason(String season) {
+        return ((root, query, criteriaBuilder) -> {
+            if (season == null) {
+                return criteriaBuilder.conjunction();
+            }
+
+            return criteriaBuilder.equal(root.get("season"), season);
+        });
+    }
+
+    public static Specification<CompetitionDAO> byCompetitionType(List<CompetitionTypeEnum> competitionTypes) {
+        return ((root, query, criteriaBuilder) -> {
+            if (competitionTypes == null || competitionTypes.isEmpty()) {
+                return criteriaBuilder.conjunction();
+            }
+
+            return root.get("type").in(competitionTypes);
+        });
+    }
+}
