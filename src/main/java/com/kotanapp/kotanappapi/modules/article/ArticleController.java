8956fd3fba96d7d96d1b93ad87addefe38fdf8cd
Roman Beznosenko <r.beznosenko@smartsoftsolutions.pl>
2026-04-23 12:09:50 +0200

    created endpoint to get article by id

--- src/main/java/com/kotanapp/kotanappapi/modules/article/ArticleController.java
+++ src/main/java/com/kotanapp/kotanappapi/modules/article/ArticleController.java
@@ -1,8 +1,11 @@
 package com.kotanapp.kotanappapi.modules.article;
 
 import com.kotanapp.kotanappapi.modules.article.models.ArticleListResponse;
+import com.kotanapp.kotanappapi.modules.article.models.ArticleResponse;
+import com.kotanapp.kotanappapi.modules.article.services.ArticleGetService;
 import com.kotanapp.kotanappapi.modules.article.services.ArticleListService;
 import com.kotanapp.kotanappapi.utils.CustomPaginationResponse;
+import com.kotanapp.kotanappapi.utils.CustomResponse;
 import com.kotanapp.kotanappapi.utils.enums.ArticleCategoryEnum;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
@@ -11,11 +14,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping(value = "/api/public/article")
 @RequiredArgsConstructor
 public class ArticleController {
     private final ArticleListService articleListService;
+    private final ArticleGetService articleGetService;
 
     private static final String DEFAULT_RESPONSE = "Operation successful.";
 
@@ -33,5 +39,19 @@ public class ArticleController {
         CustomPaginationResponse<ArticleListResponse> response = articleListService.listArticles(page, limit, category);
 
         return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{articleId}")
+    @Operation(
+            description = "Get article by id",
+            summary = "Get article by id"
+    )
+    @PreAuthorize("permitAll()")
+    public ResponseEntity<CustomResponse<ArticleResponse>> getArticleById(
+            @PathVariable(name = "articleId")UUID articleId
+    ) {
+        ArticleResponse response = articleGetService.getArticle(articleId);
+
+        return new ResponseEntity<>(new CustomResponse<>(response, DEFAULT_RESPONSE, HttpStatus.OK), HttpStatus.OK);
     }
 }
